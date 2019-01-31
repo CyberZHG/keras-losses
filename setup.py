145a927dcec5b478937e46e7a13761af6e732b53
CyberZHG <CyberZHG@gmail.com>
2019-01-31 09:10:22 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
-from setuptools import setup
+import codecs
+from setuptools import setup, find_packages
+
+
+with codecs.open('README.md', 'r', 'utf8') as reader:
+    long_description = reader.read()
+
+
+with codecs.open('requirements.txt', 'r', 'utf8') as reader:
+    install_requires = list(map(lambda x: x.strip(), reader.readlines()))
+
 
 setup(
     name='keras-losses',
-    version='0.2',
-    packages=['keras_losses'],
+    version='0.3.0',
+    packages=find_packages(),
     url='https://github.com/CyberZHG/keras-losses',
     license='MIT',
     author='CyberZHG',
     author_email='CyberZHG@gmail.com',
     description='Some loss functions in Keras',
-    long_description=open('README.rst', 'r').read(),
-    install_requires=[
-        'numpy',
-        'Keras',
-    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=install_requires,
     classifiers=(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
